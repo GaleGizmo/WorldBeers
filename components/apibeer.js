41c0721e12952d37edb10818e7a42c0d46a4db12
fpascuales <fpascuales@gmail.com>
2023-04-27 21:07:27 +0200

    Apibeer updated apibeer.js & index.js

--- components/apibeer.js
+++ components/apibeer.js
@@ -1,5 +1,5 @@
 export async function getApiBeers(){
     const resultFetch = await fetch('https://api.punkapi.com/v2/beers');
     const resultJson = await resultFetch.json();
-    console.log(resultJson);
+    return resultJson;
 }
